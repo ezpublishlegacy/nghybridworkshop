3a963d7c059082f9692d4b816b52fc57b249aa32
Netgen <info@netgen.hr>
2014-08-28 15:50:45 +0200

    Step 1 - service in legacy module

--- modules/ngpdf/download.php
+++ modules/ngpdf/download.php
@@ -2,6 +2,17 @@
 $Module = $Params['Module'];
 $nodeID = (int) $Params['node_id'];
 
+$container = ezpKernel::instance()->getServiceContainer();
+$htmlpdfapi = $container->get( 'netgen_html_pdf_api' );
+$ezcontent = $container->get( 'ez_content' );
+
+$html = $ezcontent->viewLocation($nodeID, 'full')->getContent();
+$params = array('html' => $html);
+$pdfFile = $htmlpdfapi->generateFromHTML($params);
+
+header('Content-Type: application/pdf');
+header('Content-Disposition: attachment;filename='.$nodeID.'.pdf');
+echo $pdfFile->getBody(true);
 
 eZExecution::cleanExit();
 
